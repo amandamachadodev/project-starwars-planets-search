afd7a908cab01708a3baf1397e4dcfafee5ff8ac
mandioquynha <machado.amandag@gmail.com>
2022-09-19 17:49:50 -0300

    feat: aplicação estilizada

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,10 +4,17 @@ import Header from '../Componentes/Header';
 
 function Home() {
   return (
-    <div>
-      <Header />
-      <Planets />
-    </div>
+    <>
+      <div className="title">
+        <img alt="baby-yoda" src="https://media3.giphy.com/media/z3iN1cvskVRqhf6PLf/giphy.gif?cid=ecf05e47m73dju610231rirhqrad54dkxcfygn9f4zlq6w23&rid=giphy.gif&ct=s" />
+        <p>Explore os planetas</p>
+      </div>
+      <div>
+        <Header />
+        <Planets />
+      </div>
+      <h1>Que a força esteja com você!</h1>
+    </>
   );
 }
 
