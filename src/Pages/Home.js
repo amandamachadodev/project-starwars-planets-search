c3401bc213a12bb0edfbbde210f24003d48f8b6b
mandioquynha <machado.amandag@gmail.com>
2022-09-20 23:11:18 -0300

    fix: :bug: padrão de linguagem e melhorias

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -7,13 +7,13 @@ function Home() {
     <>
       <div className="title">
         <img alt="baby-yoda" src="https://media3.giphy.com/media/z3iN1cvskVRqhf6PLf/giphy.gif?cid=ecf05e47m73dju610231rirhqrad54dkxcfygn9f4zlq6w23&rid=giphy.gif&ct=s" />
-        <p>Explore os planetas</p>
+        <p>Explore the planets</p>
       </div>
       <div>
         <Header />
         <Planets />
       </div>
-      <h1>Que a força esteja com você!</h1>
+      <h1>May the force be with you!</h1>
     </>
   );
 }
